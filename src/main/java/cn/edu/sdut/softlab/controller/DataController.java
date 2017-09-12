8d8d1cb1dd4e726ddb76ae55b4d6a5e61382340e
Anxiaolu <1466022881@qq.com>
2017-09-12 22:27:54 +0800

    更换文件生成方式

--- src/main/java/cn/edu/sdut/softlab/controller/DataController.java
+++ src/main/java/cn/edu/sdut/softlab/controller/DataController.java
@@ -9,26 +9,14 @@ import cn.edu.sdut.softlab.entity.Data;
 import cn.edu.sdut.softlab.repository.DataRepository;
 import cn.edu.sdut.softlab.util.CsvUtil;
 import com.alibaba.fastjson.JSON;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -125,30 +113,41 @@ public class DataController {
 
     @RequestMapping(value = "/download")
     @ResponseBody
-    public ResponseEntity<byte[]> download(@RequestParam("company") String Company,
+    public void download(
+            @RequestParam("company") String Company,
             @RequestParam("league") String League,
             @RequestParam("year") String Year,
             @RequestParam("match") String Match,
-            HttpServletRequest request,
-            Model model) throws Exception {
-
-        String filename = "download.csv";
-
+            HttpServletResponse response) throws Exception {
+        String s = "attachment; filename=" + combineFileName(Company, League, Year, Match);
+        String fileName = new String(s.getBytes("UTF-8"),"iso-8859-1");//解决文件名中文乱码问题,勿改
+        
         List<Data> datas = this.getDataList(Company, League, Year, Match);
-        ByteArrayOutputStream baos =(ByteArrayOutputStream) new CsvUtil().process(datas);
-        baos.close();
+        response.setHeader("Content-Disposition", fileName);
+        response.setContentType("application/csv");
+        CsvUtil csvUtil = new CsvUtil();
+        csvUtil.process(datas, response.getOutputStream());
+    }
 
-        //下载文件路径
-        HttpHeaders headers = new HttpHeaders();
-        //下载显示的文件名，解决中文名称乱码问题  
-        //String downloadFielName = new String(filename.getBytes("UTF-8"),"UTF-8");
-//        String downloadFielName = new String(filename.getBytes(""), "iso-8859-1");
-        //通知浏览器以attachment（下载方式）打开图片
-//        headers.setContentDispositionFormData("attachment", downloadFielName);
-        headers.setContentDispositionFormData("attachment", filename);
-        //application/octet-stream ： 二进制流数据（最常见的文件下载）。
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        return new ResponseEntity<byte[]>(baos.toByteArray(),
-                headers, HttpStatus.CREATED);
+    private String combineFileName(String company, String league, String year, String match) {
+        StringBuilder sb = new StringBuilder();
+        if (!(company == null || company.equals(""))) { // company not null ,append(company)
+            sb.append(company.trim());
+            if (!(league == null || league.equals(""))) { // league not null ,append(league)
+                sb.append(league.trim());
+                if (!(year == null || year.equals(""))) { // year not null ,append(year)
+                    sb.append(year.trim());
+                    if (!(match == null || match.equals(""))) { // match not null ,append(match)
+                        sb.append(match.trim());
+                    }
+                }
+            }
+            sb.append(".csv");
+
+        } else {// all null
+            sb.append("所有记录.csv");
+        }
+
+        return sb.toString();
     }
 }
