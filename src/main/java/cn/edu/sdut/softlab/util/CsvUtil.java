408bd7499c1e931c30215771e6cde290078543c6
Anxiaolu <1466022881@qq.com>
2017-09-13 11:41:42 +0800

    删除多余判断

--- src/main/java/cn/edu/sdut/softlab/util/CsvUtil.java
+++ src/main/java/cn/edu/sdut/softlab/util/CsvUtil.java
@@ -8,14 +8,10 @@ package cn.edu.sdut.softlab.util;
 import cn.edu.sdut.softlab.entity.Data;
 import cn.edu.sdut.softlab.entity.Odds;
 import com.csvreader.CsvWriter;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.List;
-//import org.apache.commons.lang.ArrayUtils;
 
 /**
  *
@@ -25,31 +21,27 @@ public class CsvUtil {
 
     /**
      * .
-     * ByteArrayOutputStream coverte to byteArray
+     *
      * @param datalist
-     * @return 
-     * @throws IOException 
+     * @param output
+     * @throws IOException
      */
-    public ByteArrayOutputStream process(List<Data> datalist)throws IOException{
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        CsvWriter csvWriter = new CsvWriter(output, ',', Charset.forName("UTF-8"));
-            String[] tableheader = {"Company", "League", "Year", "Match", "Win", "Draw", "Lose", "Rate", "UpdateTime"};
-            csvWriter.writeRecord(tableheader);
-            for (Data data : datalist) {
-                if (!(data.getOdds() == null)) {
-                    for (Odds odd : data.getOdds()) {
-                            csvWriter.writeRecord(this.getDataOdd(data, odd));
-                        }
-                    }
-            csvWriter.writeRecord(this.getData(data));
+    public void process(List<Data> datalist, OutputStream output) throws IOException {
+        CsvWriter csvWriter = new CsvWriter(output, ',', Charset.forName("GB2312"));//文件内字符中文解决,勿改
+        String[] tableheader = {"Company", "League", "Year", "Match", "Win", "Draw", "Lose", "Rate", "UpdateTime"};
+        csvWriter.writeRecord(tableheader);
+        for (Data data : datalist) {
+            for (Odds odd : data.getOdds()) {
+                csvWriter.writeRecord(this.getDataOdd(data, odd));
             }
-            csvWriter.close();
-            output.close();
-        return output;
+        }
+        csvWriter.close();
+        output.flush();
+        output.close();
     }
-    
+
     public String[] getDataOdd(Data d, Odds o) {
-        String[] arry = {d.getCompany(), d.getLeague(), d.getYear(), d.getMatch(),o.getWin(), o.getDraw(), o.getLose(), o.getReturnRate(), o.getUpdateTime()} ;
+        String[] arry = {d.getCompany(), d.getLeague(), d.getYear(), d.getMatch(), o.getWin(), o.getDraw(), o.getLose(), o.getReturnrate().toString(), o.getUpdatetime().toString()};
         return arry;
     }
 
