edf5c35a978160f191a1f84f6f635c037b374415
Anxiaolu <1466022881@qq.com>
2017-09-25 14:18:02 +0800

    修改输出时间格式

--- src/main/java/cn/edu/sdut/softlab/util/DateUtil.java
+++ src/main/java/cn/edu/sdut/softlab/util/DateUtil.java
@@ -115,7 +115,7 @@ public class DateUtil {
         Date strtodate = formatter.parse(strDate, pos);
         return strtodate;
     }
-
+    
     /**
      * 将长时间格式时间转换为字符串 yyyy-MM-dd HH:mm:ss
      *
@@ -127,7 +127,7 @@ public class DateUtil {
         String dateString = formatter.format(dateDate);
         return dateString;
     }
-
+    
     /**
      * 将短时间格式时间转换为字符串 yyyy-MM-dd
      *
@@ -849,6 +849,27 @@ public class DateUtil {
             e.printStackTrace();
         }
         return i;
+    }
+    
+    public static long getDatePoor(Date endDate, Date nowDate) {
+
+        long nd = 1000 * 24 * 60 * 60;
+        long nh = 1000 * 60 * 60;
+        long nm = 1000 * 60;
+        // long ns = 1000;
+        // 获得两个时间的毫秒时间差异
+        long diff = (endDate.getTime() - nowDate.getTime())/1000;
+        // 计算差多少天
+        long day = diff / nd;
+        // 计算差多少小时
+        long hour = diff % nd / nh;
+        // 计算差多少分钟
+        long min = diff % nd % nh / nm;
+        // 计算差多少秒//输出结果
+        // long sec = diff % nd % nh % nm / ns;
+        
+        //return day + "天" + hour + "小时" + min + "分钟";
+        return diff;
     }
 
     public String[] getFiveDate() {
